b2445b93f605945dbeeb2538f78a9ef81f9e7c95
igor-lid <onaj.igor@gmail.com>
2020-10-29 14:22:04 +0100

    add new client setup

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.scss';
+
+import { AppProps } from 'next/app';
+import { FC } from 'react';
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
