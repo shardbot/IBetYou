3d7586932e3a1306dcbc494689f8fa48df07c740
igor-lid <onaj.igor@gmail.com>
2020-11-04 17:59:05 +0100

    add step component, steps data, styling, arrows img

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,23 +1,18 @@
 import { FC } from 'react';
 
 import { MainLayout } from '../components/layouts';
+import { StepList } from '../components/steps';
+import { ARROWS_IMG_SRC } from '../constants';
+import steps from '../data/steps.json';
+import styles from '../styles/modules/pages/Home.module.scss';
 import { PageWithLayout } from '../types';
 
 const Home: FC = () => {
   return (
     <>
-      <section
-        style={{
-          marginTop: '16rem'
-        }}>
+      <div className={styles.mainWrapper}>
         {/*  FIRST PARAGRAPH */}
-        <p
-          style={{
-            color: '#fff',
-            textAlign: 'center',
-            fontSize: '1.813em',
-            lineHeight: '1.55'
-          }}>
+        <p className={styles.text}>
           Everyone has that arrogant friend who is always sure he is 100% right. Sometimes its your
           spouse, sometimes it’s a loud annoying colleague. The one that thinks he knows everything
           about MMA, NBA, football, politics. If only he was the coach, prime minister.. if they
@@ -25,27 +20,34 @@ const Home: FC = () => {
           simple way to shut them up. How? Read on
         </p>
         {/* DIVIDER */}
-        <span
-          style={{
-            display: 'block',
-            width: '40%',
-            borderTop: 'solid 1px #ffc400',
-            margin: '4rem auto'
-          }}
-        />
-        <p
-          style={{
-            color: '#fff',
-            textAlign: 'center',
-            fontSize: '1.813em',
-            lineHeight: '1.55'
-          }}>
+        <span className={styles.divider} />
+        {/*  FIRST PARAGRAPH */}
+        <p className={styles.text}>
           She thinks you will never quit smoking? Why not bet on it? He thinks Manchester would be
           the next UEFA champion? Why not bet on it? Connor is better than Khabib? Put your money
           where your mouth is! BTC will be 100.000$ by the end of 2021. Prove your conviction and
           make it interesting mo%’&%’&er!
         </p>
-      </section>
+        {/*  ARROWS */}
+        <img className={styles.arrows} src={ARROWS_IMG_SRC} alt="Arrows down" />
+        {/*  VIDEO PLACEHOLDER*/}
+        <div
+          style={{
+            background: '#fff',
+            margin: '0 8rem',
+            height: '550px'
+          }}
+        />
+      </div>
+      {/* STEPS */}
+      <div className={styles.stepsWrapper}>
+        <StepList steps={steps} />
+      </div>
+
+      {/*BOTTOM*/}
+      <div>
+        <p className={styles.text}>I BET YOU that you will click on the button below this text!</p>
+      </div>
     </>
   );
 };
