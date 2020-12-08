3c8a674eda3ae4a7d5e47b039785216dae57c8d9
Marko Ivanković <marko.ivankovic@shardlabs.io>
2020-12-08 15:20:38 +0100

    fix truffle-config.js, add natspec

--- blockchain/contracts/BetFactory.sol
+++ blockchain/contracts/BetFactory.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
 
-import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
-import {
-  ReentrancyGuard
-} from '@openzeppelin/contracts/utils/ReentrancyGuard.sol';
+// Libraries
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import  {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./Bet.sol";
 
 contract BetFactory is Ownable, ReentrancyGuard {
-    event BetDeployed(address _deployedBet);
     //----------------------------------------
     // State variables
     //----------------------------------------
@@ -18,7 +17,8 @@ contract BetFactory is Ownable, ReentrancyGuard {
     // Constructor
     //----------------------------------------
     constructor() {}
-
+    
+    event BetDeployed(address _deployedBet);
     //----------------------------------------
     // External functions
     //----------------------------------------
@@ -30,11 +30,13 @@ contract BetFactory is Ownable, ReentrancyGuard {
     function createBet(uint deposit, string memory description, uint expirationTime) external nonReentrant returns(Bet) {
         Bet newBet = new Bet(this.owner(), deposit, description, expirationTime);
         bets.push(newBet);
-        emit BetDeployed(newBet);
+        emit BetDeployed(address(newBet));
         return newBet;
     }
-    
-    function getBets() public view returns  (Bet[] memory) {
+    /**
+     * @notice Returns an array of all deployed bet instances
+     */
+    function getBets() public view returns(Bet[] memory) {
         return bets;
     }
 
