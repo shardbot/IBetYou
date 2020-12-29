3b2a0f91166d5c569e8cec6df2eb5241ce6ea7a3
Marko Ivanković <marko.ivankovic@shardlabs.io>
2020-12-29 19:50:38 +0100

    update mailing service

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,8 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { mailer, EmailTemplate } from "./services/EmailService";
-import pug from "pug";
-import path from "path";
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,38 +9,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.post("/api/invitation", (req, res) => {
-  const data = req.body;
-  const judgeParam = data.judgeType && `&type=${data.judgeType}`;
-  let link, msg, btnText = null;
+	const data = req.body;
+	const judgeParam = data.judgeType && `&type=${data.judgeType}`;
+	let link,
+		msg,
+		btnText = null;
 
-  if (data.type === "counter-bettor") {
-    link = `https://ibetyou.me/accept-bet?address=${data.betAddress}`;
-    msg = "I invite you to bet me!";
-    btnText = "Accept the Bet";
-  } else {
-    link = `https://ibetyou.me/judge?address=${data.betAddress}${judgeParam}`;
-    msg = "I invite you to be a judge!";
-    btnText = "Accept to be a judge";
-  }
+	if (data.type === "counter-bettor") {
+		link = `https://ibetyou.me/accept-bet?address=${data.betAddress}`;
+		msg = "I invite you to bet me!";
+		btnText = "Accept the Bet";
+	} else {
+		link = `https://ibetyou.me/judge?address=${data.betAddress}${judgeParam}`;
+		msg = "I invite you to be a judge!";
+		btnText = "Accept to be a judge";
+	}
+	mailer
+		.send({
+			from: "no-reply@ibetyou.me",
+			to: `${data.email}`,
+			subject: `IBetYou Invitation`,
+			html: EmailTemplate(msg, link, btnText),
+		})
+		.then(
+			() => {},
+			(error) => {
+				console.error(error);
 
-  mailer
-    .sendMail({
-      from: "no-reply@ibetyou.me",
-      to: `${data.email}`,
-      subject: `IBetYou Invitation`,
-      html: EmailTemplate(msg, link, btnText),
-    })
-    .then(() => {
-      console.log("Email sent successfully.");
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      res.sendStatus(400);
-      console.log("Email was not sent.");
-    });
+				if (error.response) {
+					console.error(error.response.body);
+				}
+			}
+		);
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server started at port: ${PORT}`);
+	console.log(`Server started at port: ${PORT}`);
 });
