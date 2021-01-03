43ffb37a434a7702aa8bd88eafe4bd44a956b778
Marko Ivanković <marko.ivankovic@shardlabs.io>
2021-01-03 17:00:11 +0100

    Reformatted code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,7 @@ app.use(cors());
 app.post("/api/invitation", (req, res) => {
 	const data = req.body;
 	const judgeParam = data.judgeType && `&type=${data.judgeType}`;
-	let link,
-		msg,
-		btnText = null;
+	let link, msg, btnText = null;
 
 	if (data.type === "counter-bettor") {
 		link = `https://ibetyou.me/accept-bet?address=${data.betAddress}`;
@@ -24,18 +22,16 @@ app.post("/api/invitation", (req, res) => {
 		msg = "I invite you to be a judge!";
 		btnText = "Accept to be a judge";
 	}
-	mailer
-		.send({
-			from: "no-reply@ibetyou.me",
-			to: `${data.email}`,
-			subject: `IBetYou Invitation`,
-			html: EmailTemplate(msg, link, btnText),
-		})
-		.then(
-			() => {
-				res.sendStatus(200);
-				console.log("Email sent successfully.");
-			},
+	mailer.send({
+		from: "no-reply@ibetyou.me",
+		to: `${data.email}`,
+		subject: `IBetYou Invitation`,
+		html: EmailTemplate(msg, link, btnText)
+	})
+		.then(() => {
+			res.sendStatus(200);
+			console.log("Email sent successfully.");
+		},
 			(error) => {
 				res.sendStatus(400);
 				console.error(error);
